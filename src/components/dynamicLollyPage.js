e7f264eeecb9217ef07128ae4121d82b548f28de
subhan300 <64373402+subhan300@users.noreply.github.com>
2020-12-27 23:52:15 +0500

    Update dynamicLollyPage.js

--- src/components/dynamicLollyPage.js
+++ src/components/dynamicLollyPage.js
@@ -2,19 +2,20 @@ import React from 'react'
 import Lolly  from "./lolly"
 function DynamicLollyPage({pageContext}) {
 
-    console.log(pageContext.ALL_DATA,"pagecontext")
-    const obj=pageContext.ALL_DATA
-    console.log(obj,"obj",obj.message)
+    // console.log(pageContext.ALL_DATA,"pagecontext")
+    // const obj=pageContext.ALL_DATA
+    // console.log(obj,"obj",obj.message)
 
     
     return (
         <div>
-            <h1>dynamic page </h1>
+            <h1>test this</h1>
+            {/* <h1>dynamic page </h1>
             <h5>{obj.sendersName}</h5>
            
             <div>
                 <Lolly top={obj.flavorTop}  middle={obj.flavorMid}  bottom={obj.flavorBot} />
-                </div>
+                </div> */}
 
 </div>
 
