4e4a308ef1d2d77ad2927a78676b3c2f8b245501
subhan300 <64373402+subhan300@users.noreply.github.com>
2020-12-27 23:13:05 +0500

    sk

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,6 +1,16 @@
 module.exports = {
   plugins: [
-   
+    {
+      resolve: "gatsby-source-graphql",
+      options: {
+        // This type will contain remote schema Query type
+        typeName: "Lollies",
+        // This is the field under which it's accessible
+        fieldName: "LOLLIES",
+        // URL to query from
+        url: "https://brave-colden-8e28fe.netlify.app/.netlify/functions/newLolly",
+      },
+    },
 
     
   ],
